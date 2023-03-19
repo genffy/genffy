6320c82a05478ce6709ff6a6e88e12d67d0ebd9c
genffy <genffyl@gmail.com>
2023-03-20 01:03:28 +0800

    feat: add ali-oss upload

---
+++ __test__/index.md
@@ -0,0 +1,9 @@
+# My Markdown Document
+
+Here is an image:
+
+![My Image](./static/1024x1024.png)
+
+And here is another image:
+
+![Another Image](./static/Screenshot-2022-12-27-at-22.37.26.png)
--- scripts/upload.mjs
+++ scripts/upload.mjs
@@ -1,43 +1,47 @@
 import fs from 'node:fs'
+import path from 'node:path'
 import * as dotenv from 'dotenv'
 dotenv.config()
 import fg from 'fast-glob';
+import OSS from 'ali-oss';
+const { ALI_OSS_REGION, ALI_OSS_KEY_ID, ALI_OSS_KEY_SECRET, ALI_OSS_BUCKET } = process.env;
+if (!ALI_OSS_REGION || !ALI_OSS_KEY_ID || !ALI_OSS_KEY_SECRET || !ALI_OSS_BUCKET) {
+  throw Error('pls config oss');
+}
 // read config first
-console.log(process.env.CDN_APP_KEY, process.env.CDN_APP_TOKEN)
-// scan all `md` file collect `relative` static files
-const folders = ['content', 'partials', 'archetypes']
-const IMAGE_REGX = /\]\((.+)(?=(\.(svg|gif|png|jpe?g)))/g
-fg(folders.map(f=>`${f}/**/*.md`), { dot: true }).then((entries)=>{
-    entries.forEach(path=>{
-        const filePath = `${process.cwd()}/${path}`;
-        console.log('filePath', filePath)
-        // fs.readFile takes the file path and the callback
-        fs.readFile(filePath, (err, data) => {
-            // if there's an error, log it and return
-            if (err) {
-                console.error(err)
-                return
-            }
-            // const files = data.toString().matchAll(IMAGE_REGX);
-            // console.log(files)
-            // Print the string representation of the data
-            // const replacedText = data.toString().replace(IMAGE_REGX, (fullResult, imagePath) => {
-            //     const newImagePath = url.resolve('http://www.example.org', imagePath)
-            //     return `](${newImagePath}`;
-            // })
-        })
-        // 
-        // content = content.replace(
-        //     '](' + matches[2],
-        //     `](/images/${slug}/${matches[2].replace(/ /g, '-').replace(/\//g, '')}`
-        // )
-        // const replacedText = data.toString().replace(regex, (fullResult, imagePath) => {
-        //     const newImagePath = url.resolve('http://www.example.org', imagePath)
-        //     return `](${newImagePath}`;
-        // })
-    })
+const client = new OSS({
+  // yourregion填写Bucket所在地域。以华东1（杭州）为例，Region填写为oss-cn-hangzhou。
+  region: ALI_OSS_REGION,
+  // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+  accessKeyId: ALI_OSS_KEY_ID,
+  accessKeySecret: ALI_OSS_KEY_SECRET,
+  bucket: ALI_OSS_BUCKET,
 });
-    
-// upload it to cdn and replace it with CDN url
 
-// done!
+// scan all `md` file collect `relative` static files
+const folders = ['content', 'partials', 'archetypes', '__test__']
+fg(folders.map(f => `${f}/**/*.md`), { dot: true }).then((entries) => {
+  entries.forEach(md => {
+    const filePath = `${process.cwd()}/${md}`;
+    fs.readFile(filePath, (err, data) => {
+      // if there's an error, log it and return
+      if (err) {
+        console.error(err)
+        return
+      }
+      let content = data.toString();
+      const matches = content.match(/(!\[.*?\]\()(.+?)(\))/g);
+      if (matches) {
+        const imagePaths = matches.map(match => match.match(/!\[.*?\]\((.*?)\)/)[1]);
+        // upload it to cdn and replace it with CDN url
+        imagePaths.forEach(async (file) => {
+          const { url, res } = await client.put(path.basename(file), path.join(path.dirname(filePath), file)/** ,{headers} */);
+          if (res && res.status === 200) {
+            content = content.replace(new RegExp(file, 'g'), url);
+            fs.writeFileSync(filePath, content);
+          }
+        })
+      }
+    })
+  })
+});
